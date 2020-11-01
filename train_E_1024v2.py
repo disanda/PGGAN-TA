b5621ea6d109f0e5b700aee37991ca1666c46b4f
disanda <skyanda1@qq.com>
2020-11-02 03:33:17 +0800

    x

--- train_E_1024v2.py
+++ train_E_1024v2.py
@@ -13,7 +13,7 @@ from torch.autograd import Variable
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #----------------path setting---------------
-resultPath = "./result/RC_Training_D_V3"
+resultPath = "./result/RC_Training_D_V3_2"
 if not os.path.exists(resultPath):
     os.mkdir(resultPath)
 
@@ -187,11 +187,13 @@ del netD1
 
 # --------------training with generative image------------share weight: good result!------------step2:no share weight:
 optimizer = torch.optim.Adam(netD2.parameters(), lr=0.001 ,betas=(0, 0.99), eps=1e-8)
-loss = torch.nn.MSELoss()
+loss_l2 = torch.nn.MSELoss()
+loss_kl = nn.KLDivLoss(size_average=False, reduce=False) #衡量分布
+loll_l1 = nn.L1Loss()
 loss_all=0
 for epoch in range(10):
 	for i in range(5001):
-		z = torch.randn(8, 512).to(device)
+		z = torch.randn(12, 512).to(device)
 		with torch.no_grad():
 			x = netG(z,depth=8,alpha=1)
 		z_ = netD2(x.detach(),height=8,alpha=1)
@@ -199,10 +201,10 @@ for epoch in range(10):
 		z_ = z_.squeeze(2).squeeze(2)
 		x_ = netG(z_,depth=8,alpha=1)
 		optimizer.zero_grad()
-		loss_1 = loss(x_,x)
-		loss_2 = loss(z.mean(),z_.mean())
-		loss_3 = loss(z.std(),z_.std())
-		loss_i = loss_1+0.5*loss_2+0.5*loss_3
+		loss_1 = loss_kl(x_,x)
+		loss_2 = loss_l2(z.mean(),z_.mean())
+		loss_3 = loss_l1(z.std(),z_.std()) #稀疏
+		loss_i = 10*loss_1+0.1*loss_2+0.1*loss_3
 		loss_i.backward()
 		optimizer.step()
 		loss_all +=loss_i.item()
@@ -212,6 +214,7 @@ for epoch in range(10):
 			torchvision.utils.save_image(img, resultPath1_1+'/ep%d_%d.jpg'%(epoch,i), nrow=8)
 			with open(resultPath+'/Loss.txt', 'a+') as f:
 				print(str(epoch)+'-'+str(i)+'-'+'loss_all__:  '+str(loss_all)+'     loss_i:    '+str(loss_i.item()),file=f)
+				print(str(epoch)+'-'+str(i)+'-'+'loss_1:  '+str(loss_1)+'  loss_2:  '+str(loss_2.item())+'  loss_3:  '+str(loss_3.item()),file=f)
 			with open(resultPath+'/D_z.txt', 'a+') as f:
 				print(str(epoch)+'-'+str(i)+'-'+'D_z:  '+str(z_[0,0:30])+'     D_z:    '+str(z_[0,30:60]),file=f)
 				print(str(epoch)+'-'+str(i)+'-'+'D_z_mean:  '+str(z_.mean())+'     D_z_std:    '+str(z_.std()),file=f)
